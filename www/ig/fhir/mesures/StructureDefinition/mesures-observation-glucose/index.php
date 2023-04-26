904336e3180fa44cd141cded49b7cc811e47fdeb
Nicolas Riss <48218773+nriss@users.noreply.github.com>
2023-04-26 10:30:01 +0200

    add mesures 3.0.1

---
+++ www/ig/fhir/mesures/StructureDefinition/mesures-observation-glucose/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/StructureDefinition-mesures-observation-glucose.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/StructureDefinition-mesures-observation-glucose.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/StructureDefinition-mesures-observation-glucose.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/StructureDefinition-mesures-observation-glucose.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/StructureDefinition-mesures-observation-glucose.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/StructureDefinition-mesures-observation-glucose.html');
+else 
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/StructureDefinition-mesures-observation-glucose.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
