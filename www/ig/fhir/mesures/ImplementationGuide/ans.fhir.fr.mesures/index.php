815bb7946120e50e020ee4be1aa8e09c247cd2bd
Nicolas Riss <48218773+nriss@users.noreply.github.com>
2023-05-03 12:21:50 +0200

    Nr mesures 3.0.1 (#14)
    * add ror script and ror to package-feed
    
    * add mesures 3.0.1
    
    * update messages
    
    * rm stu note

---
+++ www/ig/fhir/mesures/ImplementationGuide/ans.fhir.fr.mesures/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/ImplementationGuide-ans.fhir.fr.mesures.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/ImplementationGuide-ans.fhir.fr.mesures.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/ImplementationGuide-ans.fhir.fr.mesures.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/ImplementationGuide-ans.fhir.fr.mesures.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/ImplementationGuide-ans.fhir.fr.mesures.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/ImplementationGuide-ans.fhir.fr.mesures.html');
+else 
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/mesures/3.0.1/ImplementationGuide-ans.fhir.fr.mesures.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
