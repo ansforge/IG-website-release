d49dba5f2f43b44cbc99ec41c39fdf090d770104
Nicolas Riss <48218773+nriss@users.noreply.github.com>
2023-06-02 17:26:00 +0200

    publish annuaire ballot

---
+++ www/ig/fhir/annuaire/1.0.0-ballot/StructureDefinition/as-dr-healthcareservice-social-equipment/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/annuaire/1.0.0-ballot/StructureDefinition-as-dr-healthcareservice-social-equipment.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/annuaire/1.0.0-ballot/StructureDefinition-as-dr-healthcareservice-social-equipment.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/annuaire/1.0.0-ballot/StructureDefinition-as-dr-healthcareservice-social-equipment.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/annuaire/1.0.0-ballot/StructureDefinition-as-dr-healthcareservice-social-equipment.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/annuaire/1.0.0-ballot/StructureDefinition-as-dr-healthcareservice-social-equipment.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/annuaire/1.0.0-ballot/StructureDefinition-as-dr-healthcareservice-social-equipment.html');
+else 
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/annuaire/1.0.0-ballot/StructureDefinition-as-dr-healthcareservice-social-equipment.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
