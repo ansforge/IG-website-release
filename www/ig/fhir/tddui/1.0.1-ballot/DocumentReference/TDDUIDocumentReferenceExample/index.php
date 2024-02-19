6585492160de0dc2315aac92abb21e873a9db97a
Nicolas Riss <48218773+nriss@users.noreply.github.com>
2024-02-19 13:58:37 +0100

    add tddui 1.0.1-ballot (#45)

---
+++ www/ig/fhir/tddui/1.0.1-ballot/DocumentReference/TDDUIDocumentReferenceExample/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/tddui/1.0.1-ballot/DocumentReference-TDDUIDocumentReferenceExample.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/tddui/1.0.1-ballot/DocumentReference-TDDUIDocumentReferenceExample.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/tddui/1.0.1-ballot/DocumentReference-TDDUIDocumentReferenceExample.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/tddui/1.0.1-ballot/DocumentReference-TDDUIDocumentReferenceExample.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/tddui/1.0.1-ballot/DocumentReference-TDDUIDocumentReferenceExample.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/tddui/1.0.1-ballot/DocumentReference-TDDUIDocumentReferenceExample.html');
+else 
+  Redirect('https://interop.esante.gouv.fr/ig/fhir/tddui/1.0.1-ballot/DocumentReference-TDDUIDocumentReferenceExample.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
